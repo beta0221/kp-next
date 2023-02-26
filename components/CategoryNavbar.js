dcf35c6a51f9af5e72542d93316d327a1eb2985f
beta0221 <beta0221@gmail.com>
2023-02-26 15:59:52 +0800

    .

--- components/CategoryNavbar.js
+++ components/CategoryNavbar.js
@@ -7,7 +7,7 @@ const navigation = {
     categories: [
         {
             id: 1,
-            name: 'A類別',
+            name: '購物趣',
             featured: [
                 {
                     name: 'New Arrivals',
@@ -29,13 +29,6 @@ const navigation = {
                     items: [
                         { name: 'Tops', href: '#' },
                         { name: 'Dresses', href: '#' },
-                        { name: 'Pants', href: '#' },
-                        { name: 'Denim', href: '#' },
-                        { name: 'Sweaters', href: '#' },
-                        { name: 'T-Shirts', href: '#' },
-                        { name: 'Jackets', href: '#' },
-                        { name: 'Activewear', href: '#' },
-                        { name: 'Browse All', href: '#' },
                     ],
                 },
                 {
@@ -44,75 +37,12 @@ const navigation = {
                     items: [
                         { name: 'Watches', href: '#' },
                         { name: 'Wallets', href: '#' },
-                        { name: 'Bags', href: '#' },
-                        { name: 'Sunglasses', href: '#' },
-                        { name: 'Hats', href: '#' },
-                        { name: 'Belts', href: '#' },
                     ],
                 },
                 {
                     id: 'brands',
                     name: 'Brands',
                     items: [
-                        { name: 'Full Nelson', href: '#' },
-                        { name: 'My Way', href: '#' },
-                        { name: 'Re-Arranged', href: '#' },
-                        { name: 'Counterfeit', href: '#' },
-                        { name: 'Significant Other', href: '#' },
-                    ],
-                },
-            ],
-        },
-        {
-            id: 2,
-            name: 'B類別',
-            featured: [
-                {
-                    name: 'New Arrivals',
-                    href: '#',
-                    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
-                    imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
-                },
-                {
-                    name: 'Artwork Tees',
-                    href: '#',
-                    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
-                    imageAlt:
-                        'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
-                },
-            ],
-            sections: [
-                {
-                    id: 'clothing',
-                    name: 'Clothing',
-                    items: [
-                        { name: 'Tops', href: '#' },
-                        { name: 'Pants', href: '#' },
-                        { name: 'Sweaters', href: '#' },
-                        { name: 'T-Shirts', href: '#' },
-                        { name: 'Jackets', href: '#' },
-                        { name: 'Activewear', href: '#' },
-                        { name: 'Browse All', href: '#' },
-                    ],
-                },
-                {
-                    id: 'accessories',
-                    name: 'Accessories',
-                    items: [
-                        { name: 'Watches', href: '#' },
-                        { name: 'Wallets', href: '#' },
-                        { name: 'Bags', href: '#' },
-                        { name: 'Sunglasses', href: '#' },
-                        { name: 'Hats', href: '#' },
-                        { name: 'Belts', href: '#' },
-                    ],
-                },
-                {
-                    id: 'brands',
-                    name: 'Brands',
-                    items: [
-                        { name: 'Re-Arranged', href: '#' },
-                        { name: 'Counterfeit', href: '#' },
                         { name: 'Full Nelson', href: '#' },
                         { name: 'My Way', href: '#' },
                     ],
@@ -121,7 +51,8 @@ const navigation = {
         },
     ],
     pages: [
-        { name: '購物趣', href: '/category' },
+        { name: '金園廚房', href: '/' },
+        { name: '訂購相關', href: '/' },
         { name: '聯絡我們', href: '/contactUs' },
     ],
 }
@@ -289,7 +220,7 @@ const CategoryNavbar = () => {
                     Get free delivery on orders over $100
                 </p>
 
-                <nav aria-label="Top" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <nav aria-label="Top" className="px-4 sm:px-6 lg:px-8">
                     <div className="border-b border-gray-200">
                         <div className="flex h-16 items-center">
                             <button
